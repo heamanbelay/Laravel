458dfc49694edb51cbc2a1375eb3bc456f8681d7
heamanbelay <heamanbelay@gmail.com>
2022-08-03 13:11:12 +0300

    final

--- resources/views/components/home/about.blade.php
+++ resources/views/components/home/about.blade.php
@@ -7,7 +7,7 @@
           <div class="w-full xl:w-1/2 px-3 sm:px-4">
             <div class="py-3 sm:py-4">
               <img
-                src="{{ url('/img/myyyy.jpg') }}"
+                src="{{ url('/img/entoto.jpg') }}"
                 alt=""
                 class="rounded-2xl w-full"
               />
